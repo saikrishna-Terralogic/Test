ce6d4ce63b3be544623468c65e297d46072bfdac
sivaprasad-pappala <sivaprasad.pappala@terralogic.com>
2018-06-08 10:14:39 +0530

    changed factorial

--- Factorial.py
+++ Factorial.py
@@ -1,7 +1,7 @@
 # Program to find a factorial of a number
 
-number = int(input("Enter an integer for finding factorial : "))
+num = int(input("Enter an integer for finding factorial : "))
 factorial = 1
-for i in range(1, int(number)+1):
+for i in range(1, int(num)+1):
     factorial = i*factorial
 print("factorial : ", factorial)
